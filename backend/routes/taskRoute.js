630c2edda6d720e80dc6c103b93534a5297a2af8
ZinoTrust <zinotrust@gmail.com>
2022-08-06 16:29:40 +0100

    Seperated routes

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Task = require("../model/taskModel");
+const router = express.Router();
+
+router.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+// Create a Task
+router.post("/api/tasks", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Task = require("./model/taskModel");
+const taskRoutes = require("./routes/taskRoute");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(taskRoutes);
 
 const logger = (req, res, next) => {
   console.log("Middleware ran");
@@ -19,32 +20,7 @@ app.get("/", logger, (req, res) => {
   res.send("Welcome to the home page");
 });
 
-// Create a Task
-app.post("/api/tasks", async (req, res) => {
-  //   console.log(req.body);
-  //   res.send("Task created");
-  const task = await Task.create(req.body);
-  res.status(200).json(task);
-  try {
-    const task = await Task.create(req.body);
-    res.status(200).json(task);
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-});
-
-// Get all Tasks
-app.get("/api/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.status(200).json(tasks);
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-});
-
 const PORT = process.env.PORT || 8000;
-
 // Connect DB & start server
 mongoose
   .connect(process.env.MONGO_URI)
